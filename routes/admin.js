fe056600ac9223e10c7bf77c312e10682a1e83ad
Brian Falasz <bfalasz@yahoo.com>
2019-06-30 23:40:25 -0500

    added some html files and used sendFile to route to the correct files

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get( '/add-product',(req, res, next) => {  
-  res.send('<html><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form></html>');
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get( '/',(req, res, next) => {
-  res.send('<html>Hello from express</html>');
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
