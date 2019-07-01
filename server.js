8a22766641ffde946d51c7b2a8426c8060d15e7d
Brian Falasz <bfalasz@yahoo.com>
2019-06-30 23:51:53 -0500

    added an error page and created a path file to export the root path to the routes files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -12,7 +13,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page Not Found</h1>')
+  res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
 })
 
 app.listen(3000);
