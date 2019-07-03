1234230207adef99c87a69898d7cfc56bdb85017
Brian Falasz <bfalasz@yahoo.com>
2019-07-02 22:51:29 -0500

    moved main project files to new folder. Started working with the template engine pug

--- main/routes/shop.js
+++ main/routes/shop.js
@@ -2,11 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get( '/',(req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop');
 });
 
 module.exports = router;
