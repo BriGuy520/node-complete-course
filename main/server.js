ddfb7f25340c06db9d80726fdb52e6b9945431fe
Brian Falasz <bfalasz@yahoo.com>
2019-07-10 22:02:11 -0500

    made some ejs templates

--- main/server.js
+++ main/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const handlebars = require('express-handlebars');
+// const handlebars = require('express-handlebars');
 const path = require('path');
 
 const app = express();
 
-app.engine('hbs', handlebars({
-  layoutsDir: 'views/layouts',
-  defaultLayout: 'main-layout',
-  extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
+// app.engine('hbs', handlebars({
+//   layoutsDir: 'views/layouts',
+//   defaultLayout: 'main-layout',
+//   extname: 'hbs'
+// }));
+
+app.set('view engine', 'ejs');
+// app.set('view engine', 'hbs');
 // app.set('view engine', 'pug');
 app.set('views', 'views');
 
