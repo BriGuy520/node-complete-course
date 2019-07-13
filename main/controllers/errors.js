200dcfbea6d9f75553d3d22df11b8f55512fdb27
Brian Falasz <bfalasz@yahoo.com>
2019-07-12 22:33:56 -0500

    exported route logic to new controller file

---
+++ main/controllers/errors.js
@@ -0,0 +1,3 @@
+exports.errors = (req, res, next) => {
+  res.status(404).render('error', {docTitle: 'Page Not Found'});
+}
--- main/server.js
+++ main/server.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 const app = express();
 
+const errorController = require("./controllers/errors");
+
 // app.engine('hbs', handlebars({
 //   layoutsDir: 'views/layouts',
 //   defaultLayout: 'main-layout',
@@ -25,8 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('error', {docTitle: 'Page Not Found'});
-})
+app.use(errorController.errors);
 
 app.listen(3000);
