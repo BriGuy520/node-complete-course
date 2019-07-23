955599e99527ab3265d23d2902c5c04fd5799b03
Brian Falasz <bfalasz@yahoo.com>
2019-07-22 20:43:53 -0500

    ignored the util folder... I think

--- main/server.js
+++ main/server.js
@@ -6,6 +6,7 @@ const path = require('path');
 const app = express();
 
 const errorController = require("./controllers/errors");
+const db = require('./util/database');
 
 // app.engine('hbs', handlebars({
 //   layoutsDir: 'views/layouts',
@@ -26,6 +27,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+db.execute('SELECT * FROM products')
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(errorController.errors);
 
