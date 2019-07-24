43bf6d54a6042b2e2f104ba52fde610c72b6dd19
Brian Falasz <bfalasz@yahoo.com>
2019-07-23 19:56:45 -0500

    used sequelize to remake the cart model and created a new cart item model and connected them and established the correct relationships in the server file

--- main/server.js
+++ main/server.js
@@ -11,6 +11,8 @@ const sequelize = require('./util/database');
 
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 // app.engine('hbs', handlebars({
 //   layoutsDir: 'views/layouts',
@@ -47,13 +49,15 @@ Product.belongsTo(User,{
   constraints: true,
   onDelete: "CASCADE"
 });
-
 User.hasMany(Product);
-
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  // .sync({ force: true })
-  .sync()
+  .sync({ force: true })
+  // .sync()
   .then(result => {
     // console.log(result);
 
