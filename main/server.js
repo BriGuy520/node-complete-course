19f6dd30b9767b5aebe161c793e8f2db72a6ac5e
Brian Falasz <bfalasz@yahoo.com>
2019-07-23 19:27:28 -0500

    added a new model for users and set up a relationship with the product model in the server file

--- main/server.js
+++ main/server.js
@@ -9,6 +9,9 @@ const errorController = require("./controllers/errors");
 
 const sequelize = require('./util/database');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+
 // app.engine('hbs', handlebars({
 //   layoutsDir: 'views/layouts',
 //   defaultLayout: 'main-layout',
@@ -26,14 +29,42 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+  .then(user => {
+    req.user = user;
+  })
+  .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.errors);
 
-sequelize.sync()
+Product.belongsTo(User,{
+  constraints: true,
+  onDelete: "CASCADE"
+});
+
+
+sequelize
+  // .sync({ force: true })
+  .sync()
   .then(result => {
     // console.log(result);
+
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if(!user){
+      return User.create({name: 'Brian', email: 'test@example.com' });
+    }
+
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch(err => console.log(err));
